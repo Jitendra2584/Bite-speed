2db75c698846154948ebfeea33bed1bd6e0a7734
Jitendra Singh <jitendrasingh@Jitendras-MacBook-Air.local>
2025-07-03 06:12:49 +0530

    add-db-url

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,21 +1,19 @@
 import { DataSource } from "typeorm";
 import { Contacts } from "./entity/contacts";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST ?? "localhost",
-  port: Number(process.env.DB_PORT) || 5555,
-  username: process.env.DB_USER ?? "your_pg_username",
-  password: process.env.DB_PASSWORD ?? "your_pg_password",
-  database: process.env.DB_NAME ?? "bites_peed_identity",
+  url: process.env.DB_URL,
   synchronize: false,
-  logging: false,
+  logging: true,
   entities: [Contacts],
-  extra:{
+  ssl: true, // Enable SSL for secure connections
+  extra: {
     connectionLimit: 10, // Optional: Set a connection limit for the pool
     idleTimeoutMillis: 30000,
-  }
+    ssl: { rejectUnauthorized: false }
+  },
 });
